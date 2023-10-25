37632a84b354654bf8312ba8915239a896ff69bf
fabiosabino20 <83607086+fabiosabino20@users.noreply.github.com>
2023-10-25 16:55:02 -0300

    Swipe Controller
    Created swipe controller

---
+++ Assets/Scripts/SwipeController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(TrailRenderer), typeof(BoxCollider))]
+
+public class SwipeController : MonoBehaviour
+{
+    private bool isSwiping = false;
+    private BoxCollider boxCol;
+    private Camera cam;
+    private float swipeTimer = 0;
+    private GameObject ball;
+    private Rigidbody ballRb;
+    private TrailRenderer trail;
+    private Vector3 mouseInitialPos, mouseFinalPos, mousePos; // mousePos used for trail
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+        trail = GetComponent<TrailRenderer>();
+        boxCol = GetComponent<BoxCollider>();
+        trail.enabled = false;
+        boxCol.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isSwiping = true;
+            UpdateComponents();
+            mouseInitialPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isSwiping = false;
+            UpdateComponents();
+            mouseFinalPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1f));
+            float throwStrenght = (mouseFinalPos.y - mouseInitialPos.y)/swipeTimer;
+            ballRb.useGravity = true;
+            ballRb.AddForce(new Vector3(-throwStrenght, throwStrenght, 0), ForceMode.Impulse);
+        }
+        if (isSwiping)
+        {
+            UpdateMousePosition();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ball = other.gameObject;
+            ballRb = ball.GetComponent<Rigidbody>();
+        }
+    }
+
+    private void UpdateMousePosition()
+    {
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1.8f));
+        transform.position = mousePos;
+        if (ball != null)
+            ball.transform.position = mousePos;
+        swipeTimer += Time.deltaTime;
+    }
+
+    private void UpdateComponents()
+    {
+        trail.enabled = isSwiping;
+        boxCol.enabled = isSwiping;
+    }
+}
